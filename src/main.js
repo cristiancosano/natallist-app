f5973d196f8ad5388ba1320fecf8e29642bf5d93
cristiancosano <cristiancosano@icloud.com>
2022-03-29 00:16:37 +0200

    first commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './app.scss'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderMain></HeaderMain>
+    Home
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import HeaderMain from '@/components/HeaderMain'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    HeaderMain
   }
+
 }
 </script>
